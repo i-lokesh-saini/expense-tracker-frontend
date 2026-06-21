3400b10fa4c96a30d8c3149964420ee2aa561f32
Lokesh Saini <ls0647515@gmail.com>
2026-06-21 18:09:41 +0530

    Add files via upload

--- src/components/card/TransactionInfoCard.jsx
+++ src/components/card/TransactionInfoCard.jsx
@@ -1,14 +1,14 @@
 import { LuUtensils, LuTrendingUp, LuTrendingDown, LuTrash2 } from "react-icons/lu";
 
-const TransactionInfoCard = ({title, icon, date, amount, type, hideDeleteBtn, onDelete}) => {
-    
+const TransactionInfoCard = ({ title, icon, date, amount, type, hideDeleteBtn, onDelete }) => {
+
     const getAmountStyles = () => {
         return type === "income"
             ? "bg-green-50 text-green-500"
             : "bg-red-50 text-red-500";
     };
 
-    
+
     return (
         <div className="group relative flex items-center gap-4 mt-2 p-3 rounded-lg hover:bg-gray-100/60">
             <div className="w-12 h-12 flex items-center justify-center text-xl text-gray-800 bg-gray-100 rounded-full">
@@ -26,14 +26,14 @@ const TransactionInfoCard = ({title, icon, date, amount, type, hideDeleteBtn, on
                 </div>
                 <div className="flex items-center gap-2">
                     {!hideDeleteBtn && (
-                        <button className="text-gray-400 hover:text-red-500 opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer" onClick={onDelete}><LuTrash2 size={18} /></button>
+                        <button className="text-gray-400 hover:text-red-500 opacity-50 group-hover:opacity-100 transition-opacity cursor-pointer" onClick={onDelete}><LuTrash2 size={18} /></button>
                     )}
 
                     <div className={`flex items-center gap-2 px-3 py-1.5 rounded-md ${getAmountStyles()}`}>
-                        <h6 className="text-xs font-medium "> 
+                        <h6 className="text-xs font-medium ">
                             {type === "income" ? "+" : "-"} ₹{amount}
                         </h6>
-                        {type === "income" ? <LuTrendingUp/> : <LuTrendingDown/>}
+                        {type === "income" ? <LuTrendingUp /> : <LuTrendingDown />}
                     </div>
                 </div>
             </div>
